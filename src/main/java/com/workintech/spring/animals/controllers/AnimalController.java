c037ed24185ad661c359c253c7e7807e9650b5b4
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-11 23:58:01 +0300

    Root URL changed to /workintech/animal

--- src/main/java/com/workintech/spring/animals/controllers/AnimalController.java
+++ src/main/java/com/workintech/spring/animals/controllers/AnimalController.java
@@ -1,8 +1,13 @@
 package com.workintech.spring.animals.controllers;
 
+import com.workintech.spring.animals.entity.Animal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/workintech")
+@RequestMapping("/workintech/animal")
 public class AnimalController {
+    private Map<Integer, Animal> animals = new HashMap<>();
 }
