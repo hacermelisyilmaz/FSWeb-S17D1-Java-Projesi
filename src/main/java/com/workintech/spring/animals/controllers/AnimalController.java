5ec5cfd059019620e3c96bba4f3e7e44d81f8d6f
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-12 00:10:19 +0300

    GetMapping with getAnimal

--- src/main/java/com/workintech/spring/animals/controllers/AnimalController.java
+++ src/main/java/com/workintech/spring/animals/controllers/AnimalController.java
@@ -4,10 +4,16 @@ import com.workintech.spring.animals.entity.Animal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping("/workintech/animal")
 public class AnimalController {
     private Map<Integer, Animal> animals = new HashMap<>();
+
+    @GetMapping
+    public List<Animal> getAnimals() {
+        return animals.values().stream().toList();
+    }
 }
